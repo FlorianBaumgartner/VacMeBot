dcf71ae431b4e86bdc893bcebc655ec456ad7f33
Florian Baumgartner <florian.b-h@bluewin.ch>
2021-12-28 10:40:12 +0100

    Made updating interval more precise

--- VacmeBot.py
+++ VacmeBot.py
@@ -1,6 +1,5 @@
 import time
 import telegram_send
-# from pynput import keyboard
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
@@ -9,24 +8,22 @@ from datetime import datetime
 url = "https://boostli.web.app/"
 date_threshold = "04.01.2022"
 target_location = "Impfzentrum MEDIN Biel – mRNA"
-sleep_time = 60
+sleep_time = 40
 
 
 telegram_send.send(messages=["Start Program"])
 date_threshold = datetime.strptime(date_threshold, '%d.%m.%Y')
-# listener = keyboard.Listener(on_press=lambda x: globals().update(run=False))
-# listener.start()
 oldDate = None
-run = True
 
-while(run):
+while(True):
+    now = time.time()
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("headless")    # Open browser in background
         driver = webdriver.Chrome(ChromeDriverManager().install(),
                                   chrome_options=options)
         driver.get(url)
-        time.sleep(1)     # Give some time to fully load content
+        time.sleep(2)     # Give some time to fully load content
         
         locations = driver.find_elements_by_class_name("ng-star-inserted")
         content = [location.text for location in locations]
@@ -39,22 +36,18 @@ while(run):
         
         if(oldDate != date):
             oldDate = date
-            print("This is the first time we have seen this new date")
+            print("This is the first time we have seen this date")
             if(date < date_threshold):
                 print("We have found an appointment within threshold!")
-                message = (f"Appointment available: {date.strftime('%d.%m.%Y')}\n"
-                           "Visit now https://be.vacme.ch/start\n"
-                           "Good Luck!")
-                telegram_send.send(messages=[message])
+                ms = (f"Appointment available: {date.strftime('%d.%m.%Y')}\n"
+                       "Visit now https://be.vacme.ch/start\n"
+                       "Good Luck!")
+                telegram_send.send(messages=[ms])
     
     except Exception as e:
         print(f"Exception occured: {e}")
         
-    now = time.time()
-    while(time.time() - now < sleep_time):
-        time.sleep(0.1)
-        if(not run):
-            break
+    time.sleep(max(0, sleep_time - (time.time() - now)))
 
 
 print("Terminate Program")
